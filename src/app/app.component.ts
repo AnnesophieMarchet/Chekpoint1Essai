3178f928961b5a42ef5958547cfdb3af82df9448
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-11-05 21:58:14 +0100

    clean projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'checkpoint01';
+  title = 'Youtube Anne-Sophie';
 }
